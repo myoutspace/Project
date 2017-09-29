4d6a047c5b80080961701b7e6ce806a9c96bdd6d
moulinaparole <moulinaparole@live.ca>
2017-09-29 05:47:56 -0400

    working on device

--- NeedSomeDoSome/app/src/main/java/com/example/julien/needsomedosome/MainActivity.java
+++ NeedSomeDoSome/app/src/main/java/com/example/julien/needsomedosome/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.julien.needsomedosome;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void CreateGroupBtn()
+    public void CreateGroupBtn(View v)
     {
         startActivity(new Intent(MainActivity.this, AlexandreActivity.class));
     }
