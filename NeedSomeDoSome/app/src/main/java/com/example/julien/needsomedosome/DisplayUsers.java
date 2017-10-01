04111173aa2656305b142e8eaba909a66cf80827
Alexandre Prud'Homme <aprud009@uottawa.ca>
2017-09-30 22:48:19 -0400

    I have added the activity pages LandingPage, CreateGroup, CreateUser, and DisplayUsers

---
+++ NeedSomeDoSome/app/src/main/java/com/example/julien/needsomedosome/DisplayUsers.java
@@ -0,0 +1,13 @@
+package com.example.julien.needsomedosome;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DisplayUsers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_users);
+    }
+}
---
+++ NeedSomeDoSome/app/src/main/java/com/example/julien/needsomedosome/LandingPage.java
@@ -0,0 +1,13 @@
+package com.example.julien.needsomedosome;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LandingPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing_page);
+    }
+}
