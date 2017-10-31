eefbc35cbf8c185d2fb252596c37875a2e099a09
Production <production.outspace@gmail.com>
2017-10-31 14:21:19 -0400

    A lot of new changes

--- TaskManager/app/src/main/java/main/taskmanager/Display/MainActivity.java
+++ TaskManager/app/src/main/java/main/taskmanager/Display/MainActivity.java
@@ -5,17 +5,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import java.util.ArrayList;
+
 import main.taskmanager.R;
+import main.taskmanager.javaActions.DatabaseHelper;
 
 public class MainActivity extends AppCompatActivity {
+
+    DatabaseHelper database;
+    ArrayList<String> groups;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        database = new DatabaseHelper(this);
+        groups = database.getAllGroups();
     }
 
     public void onCreateGroup(View view){
-        startActivity(new Intent(this, CreateGroup.class));
+        if(groups.isEmpty())
+            startActivity(new Intent(this, CreateGroup.class));
+        else
+            startActivity(new Intent(this, HomePage.class));
     }
 }
