d439892c84a6a5d2a3784ccfa0044a45703cf195
Alexandre Prud'Homme <aprud009@uottawa.ca>
2017-11-11 12:08:21 -0500

    New Activity main page with timer

--- TaskManager/app/src/main/java/main/taskmanager/Display/MainActivity.java
+++ TaskManager/app/src/main/java/main/taskmanager/Display/MainActivity.java
@@ -4,7 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-
+import android.view.Window;
+import android.os.Handler;
 import java.util.ArrayList;
 
 import main.taskmanager.R;
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
 
     DatabaseHelper database;
     ArrayList<String> groups;
+    private static int SPLAH_TIME_OUT = 4000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +23,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         database = new DatabaseHelper(this);
         groups = database.getAllGroups();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if(database.getActiveGroup() == null)
+                    startActivity(new Intent(MainActivity.this, CreateGroup.class));
+                else
+                    startActivity(new Intent(MainActivity.this, HomePage.class));
+                finish();
+            }
+        },SPLAH_TIME_OUT);
     }
 
+    /*
     public void onCreateGroup(View view){
         if(database.getActiveGroup() == null)
             startActivity(new Intent(this, CreateGroup.class));
@@ -30,4 +43,5 @@ public class MainActivity extends AppCompatActivity {
             startActivity(new Intent(this, HomePage.class));
 
     }
+    */
 }
