909714e6d2ad349af06506a5fd3e04a64a9cf086
moulinaparole <moulinaparole@live.ca>
2017-11-12 12:28:24 -0500

    working on group selection page

--- TaskManager/app/src/main/java/main/taskmanager/Display/GroupSelection.java
+++ TaskManager/app/src/main/java/main/taskmanager/Display/GroupSelection.java
@@ -4,12 +4,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import main.taskmanager.R;
+import main.taskmanager.javaActions.DatabaseHelper;
 
 public class GroupSelection extends AppCompatActivity {
+
+    DatabaseHelper databaseHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group_selection);
+        databaseHelper = DatabaseHelper.getInstance(getApplicationContext());
     }
+
+
+
+
 }
---
+++ TaskManager/app/src/main/java/main/taskmanager/javaActions/GroupSelectionAdapter.java
@@ -0,0 +1,70 @@
+package main.taskmanager.javaActions;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import main.taskmanager.Display.HomePage;
+import main.taskmanager.R;
+
+/**
+ * Created by Julien on 2017-11-12.
+ */
+
+public class GroupSelectionAdapter extends ArrayAdapter<String> {
+
+    DatabaseHelper databaseHelper;
+    Context activityContext;
+
+    private static class ViewHolder {
+        private Button groupButton;
+    }
+
+    public GroupSelectionAdapter(Context context, ArrayList<String> items, DatabaseHelper databaseHelper){
+        super(context, R.layout.group_list_item,items);
+        this.databaseHelper = databaseHelper;
+        activityContext = context;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        GroupSelectionAdapter.ViewHolder viewHolder;
+
+        if (convertView == null) {
+            convertView = LayoutInflater.from(this.getContext())
+                    .inflate(R.layout.group_list_item, parent, false);
+
+            viewHolder = new GroupSelectionAdapter.ViewHolder();
+            viewHolder.groupButton = (Button) convertView.findViewById(R.id.groupButton);
+
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (GroupSelectionAdapter.ViewHolder) convertView.getTag();
+        }
+
+        final String groupName = getItem(position);
+
+        if (groupName!= null) {
+            viewHolder.groupButton.setText(groupName);
+
+            viewHolder.groupButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    databaseHelper.setActiveGroup(groupName);
+                    Intent intent = new Intent(getContext(), HomePage.class);
+                    activityContext.startActivity(intent);
+                }
+            });
+        }
+
+        return convertView;
+    }
+
+}
