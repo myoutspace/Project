966f1d9fadb50ff79beac3b6d2d372ceede8e80f
Production <production.outspace@gmail.com>
2017-11-18 18:54:36 -0500

    Push before pull

---
+++ TaskManager/app/src/main/java/main/taskmanager/javaActions/SimpleAction.java
@@ -0,0 +1,15 @@
+package main.taskmanager.javaActions;
+
+import android.view.KeyEvent;
+import android.view.View;
+
+/**
+ * Created by rukau on 2017-11-18.
+ */
+
+public class SimpleAction {
+    public static String capitalizeName(String name){
+        name = name.substring(0,1).toUpperCase() + name.substring(1,name.length());
+        return name;
+    }
+}
